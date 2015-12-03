73e5540e34a9dbe2562331cae83b18cab028ce3b
VictorWinberg <dat14vwi@student.lu.se>
2015-12-03 21:17:35 +0100

    Dropdowns added

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -10,6 +10,32 @@
 angular.module('yoangularApp')
   .controller('HeaderCtrl', function ($scope, $location) {
     $scope.isActive = function (viewLocation) {
-        return viewLocation === $location.path();
+        return '/' + viewLocation === $location.path();
     };
+    $scope.dropdowns =
+    [
+      {title: 'Aktuellt', items:
+      [
+        {title: 'Nyheter', href: 'nyheter'},
+        {title: 'Kalender', href: 'kalender'},
+        {title: 'Eventanmälan', href: 'eventanmälan'}
+      ]},
+      {title: 'Community', items:
+      [
+        {title: 'Anslut', href: 'anslut'}
+      ]},
+      {title: 'För funktionärer', items:
+      [
+        {title: 'STAB16', href: 'stab16'}
+      ]},
+      {title: 'Sektionen', items:
+      [
+        {title: 'Om sektionen', href: 'sektionen'},
+        '-',
+        {title: 'Swagtest', href: 'swagtest'},
+        {title: 'Store', href: 'store'}
+      ]},
+      {title: 'För företag', href: 'foretag'},
+      {title: 'About', href: 'about'}
+    ];
   });
